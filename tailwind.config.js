f1b9ccfa8f032cf80f95b3b7a1a550fbf6986583
Boyo <finalofme@gmail.com>
2024-04-05 10:49:25 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/App.jsx"],
+  theme: {
+    extend: {
+      colors: {
+       
+        hitam: '#000000',
+        biru: '#14213D',
+        oren: '#FCA311',
+        abu: '#E5E5E5',
+      },
+    },
+  },
+  plugins: [],
+}
+
